4ca5388bc2fa1579aa02efe406f8532c3cdf7c04
Daniel <32261508+prwn90@users.noreply.github.com>
2019-04-01 08:00:33 +0200

    Shares website project
    PHP tutorial.

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php 
+
+//Define database 
+
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "shareboard");
+
+
+//Define URL
+define("ROOT_PATH", "/shareboard/");
+define("ROOT_URL", "http://localhost/shareboard/");
+
---
+++ views/home/index.php
@@ -0,0 +1,7 @@
+<div class="text-center">
+	<h1>Welcome To ShareBoard Web!!!</h1>
+
+	<p class="lead">Join to us and shares posts and links to the WORLD!</p>
+
+	<a class="btn btn-success text-center" href="<?php echo ROOT_PATH;?>shares">Share Now!</a>
+</div>
